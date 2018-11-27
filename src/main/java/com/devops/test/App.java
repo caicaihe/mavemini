78499752a4c2562bce595575e04c0d4644f097b1
caicaihe <heliping@caicloud.io>
2018-11-27 13:29:00 +0800

    update

---
+++ src/main/java/com/devops/test/App.java
@@ -0,0 +1,13 @@
+package com.devops.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
